58b893c6787a3cac0caa839a15aa8875d6ae1d5a
daniel elers <daniel.elers@huckit.de>
2023-01-13 11:57:12 +0100

    Created YFOverviewPage file

---
+++ lib/overview_page/overview_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class YFOverviewPage extends StatelessWidget {
+  const YFOverviewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+      children: [
+        Row(
+          children: [Text('')],
+        ),
+        Row(
+          children: [],
+        ),
+      ],
+    ));
+  }
+}
