46539eae5579105f78ffb6b5e087ffbadf18453b
Thomas Drebes <38798057+tdrebes@users.noreply.github.com>
2023-01-16 11:01:33 +0100

    Adds spotify api

---
+++ lib/core/api/spotify_api.dart
@@ -0,0 +1,78 @@
+import 'dart:convert';
+
+import 'package:oauth2_client/oauth2_helper.dart';
+import 'package:oauth2_client/spotify_oauth2_client.dart';
+import 'package:yotifiy/config.dart';
+import 'package:yotifiy/playlist/playlist_model.dart';
+
+class YFSpotifyApi {
+  final _redirectUri = 'http://localhost:42070/callback';
+  final _uriScheme = 'http://localhost:42070';
+  final _scopes = ['playlist-read-private'];
+  final _endpointPlaylist = 'https://api.spotify.com/v1/playlists/';
+  final String _clientId;
+  final String _clientSecret;
+
+  YFSpotifyApi()
+      : _clientId = Config.spotifyClientId(),
+        _clientSecret = Config.spotifyClientSecret();
+
+  Future<YFPlaylist> getPlaylist() async {
+    var playlistId = '2cnKM0Cb4uL8bhQowdybmw';
+    var res = await _authHelper.get(_endpointPlaylist + playlistId);
+    var data = jsonDecode(res.body); // TODO: throw exception if invalid status
+
+    List<YFMediaItem> mediaItems = await fetchPlaylistItems(playlistId);
+
+    YFPlaylist playlist = YFPlaylist(
+      id: data['id'] ?? '',
+      playlistURL: data['external_urls']?['spotify'] ?? '',
+      thumbnailURL: data['images']?[0]?['url'] ?? '',
+      description: data['description'] ?? '',
+      name: data['name'] ?? '',
+      mediaItems: mediaItems,
+    );
+
+    print(playlist.toJson());
+
+    return playlist;
+  }
+
+  Future<List<YFMediaItem>> fetchPlaylistItems(String playlistId) async {
+    List<YFMediaItem> mediaItems = <YFMediaItem>[];
+
+    var res = await _authHelper.get('$_endpointPlaylist$playlistId/tracks');
+    var data = jsonDecode(res.body); // TODO: throw exception if invalid status
+    var items = data['items'] ?? [];
+
+    for (var item in items) {
+      YFMediaItem mediaItem = YFMediaItem(
+        id: item['track']?['id'] ?? '',
+        name: item['track']?['name'] ?? '',
+        mediaURL: item['track']?['external_urls']?['spotify'] ?? '',
+        mediaImageURL: item['track']?['album']?['images']?[0]?['url'] ?? '',
+      );
+
+      mediaItems.add(mediaItem);
+
+      print(mediaItem.toJson());
+    }
+
+    return mediaItems;
+  }
+
+  OAuth2Helper get _authHelper {
+    var helper = OAuth2Helper(
+      SpotifyOAuth2Client(
+        redirectUri: _redirectUri,
+        customUriScheme: _uriScheme,
+      ),
+      grantType: OAuth2Helper.authorizationCode,
+      clientId: _clientId,
+      clientSecret: _clientSecret,
+      scopes: _scopes,
+    );
+
+    return helper;
+  }
+}
